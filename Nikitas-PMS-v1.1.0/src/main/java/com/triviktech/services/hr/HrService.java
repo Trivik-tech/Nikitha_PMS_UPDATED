01aeb8eef2ff021a2a55c1fdc1408ca34b6c1917
Gurunath Jadhav <gurunath.jadhav.842@gmail.com>
2025-05-11 19:16:57 +0530

    handled the KraKpiNotFoundException in global exception handler

--- Nikitas-PMS-v1.1.0/src/main/java/com/triviktech/services/hr/HrService.java
+++ Nikitas-PMS-v1.1.0/src/main/java/com/triviktech/services/hr/HrService.java
@@ -1,19 +1,15 @@
 package com.triviktech.services.hr;
 
-import com.triviktech.entities.hr.HR;
-import com.triviktech.payloads.request.employee.Employee;
 import com.triviktech.payloads.request.hr.HrRequestDto;
 import com.triviktech.payloads.response.employee.EmployeeInfo;
-import com.triviktech.payloads.response.employeeslist.EmployeesList;
-import com.triviktech.payloads.response.global.Response;
+
 import com.triviktech.payloads.response.hr.HrResponseDto;
-import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
+
 
 public interface HrService {
 
