70d1daab8f4fba03fd42c7ccdd005d089e66422a
Avinash S H <aveeavinash74@gmail.com>
2025-07-03 12:46:46 +0700

    Notification count implemented

--- nikithas-pms-v1.1.0/src/components/urls/CommenUrl.js
+++ nikithas-pms-v1.1.0/src/components/urls/CommenUrl.js
@@ -1,4 +1,4 @@
-let serverUrl = "http://192.168.0.193:8080";
-// export let baseUrl = "http://localhost:8080"; // Use this during local development
-export let baseUrl= serverUrl;// Use this during local development
+// let serverUrl = "http://192.168.0.193:8080";
+export let baseUrl = "http://localhost:8080"; // Use this during local development
+// export let baseUrl= serverUrl;// Use this during local development
 
